409188157a01c8aa236c9d99e6297b16266877fc
Ángel Iniesta Sánchez <angel.iniestas@um.es>
2022-03-26 18:07:24 +0100

    Commit para adaptar a progreso Semana 4. Pendiente de errores y completar cosas.

---
+++ CampuzanoMelgarejo-FranciscoDavid-proyecto/src/main/java/ciudades/repositorio/RepositorioCiudades.java
@@ -0,0 +1,18 @@
+package ciudades.repositorio;
+
+
+import org.example.ciudades.Ciudad;
+
+import repositorio.Repositorio;
+
+public interface RepositorioCiudades extends Repositorio<Ciudad, String> {
+
+	/*
+	 * El repositorio declara los métodos básicos de gestión de la entidad.
+	 * 
+	 * En esta interfaz podemos añadir métodos más específicos.
+	 */
+	
+	
+	
+}
